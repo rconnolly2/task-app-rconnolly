e24375cf14a92cab4a84bdc809cc3df3e0555714
rconnolly2 <robertoconnolly100@gmail.com>
2025-02-27 19:56:35 +0100

    Seguir front pagina modulo en casa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { PendingTasksComponent } from './pending-tasks/pending-tasks.component';
+import { ModuleComponent } from './module/module.component';
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'calendar', component: CalendarComponent },
     { path: 'pending-tasks', component: PendingTasksComponent },
+    { path: 'module/:id', component: ModuleComponent },
     // { path: 'add-class', component: AddClassComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
---
+++ src/app/module/module.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-module',
+  imports: [],
+  templateUrl: './module.component.html',
+  styleUrl: './module.component.css'
+})
+export class ModuleComponent {
+
+}
